9ea5afec71d2ee59dcb4d1c51c2bf0b522c339ee
hhertout <hugues.hertout@gmail.com>
2024-01-07 09:46:49 +0100

    test: remove useless test

--- src/test/java/hhertout/api/ApiApplicationTests.java
+++ src/test/java/hhertout/api/ApiApplicationTests.java
@@ -1,13 +1,7 @@
 package hhertout.api;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
