14c2d85570c7bf2c9415697ef39eaaeb0712e0a3
hhertout <hugues.hertout@gmail.com>
2024-01-07 11:12:50 +0100

    feat: improve ping controller response

--- src/main/java/hhertout/api/controller/MainController.java
+++ src/main/java/hhertout/api/controller/MainController.java
@@ -1,15 +1,33 @@
 package hhertout.api.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
+    static final class PingResponse {
+        private String message;
+
+        public PingResponse(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+    }
 
     @GetMapping("/ping")
     @ResponseBody
-    public String index() {
-        return "Pong";
+    public ResponseEntity<PingResponse> index() {
+        PingResponse response = new PingResponse("pong");
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
